de3c9e2afe00c98f605231a77f52c5c8482d4414
Karan Kumar <officialkaran39@gmail.com>
2025-02-25 13:59:49 +0530

    add comment hello

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+// annotation
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
